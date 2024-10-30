e0612632507576e830dd8b8933e861ea322a9825
Jude <jm2489@njit.edu>
2024-10-30 18:08:19 -0400

    Added the logout.php that also sends the 'logout' request to the RMQ server in order to delete the session token. This will log the user out of the system and redirect them to the login page.

---
+++ IT490/Web/php/logout.php
@@ -0,0 +1,37 @@
+<?php
+// Logout script that sends the logout request to the rabbitmq server to delete token from the session for the user
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+    $request = [
+        'type' => "logout",
+        'session_token' => $_COOKIE['session'],
+        'message' => "Logout Request"
+    ];
+
+    // Send the request to RabbitMQ server
+    $response = $client->send_request($request);
+
+    // Decode the JSON response from RabbitMQ
+    $response = json_decode($response, true);
+
+    if (isset($response['success']) && $response['success']) {
+        header("Location: /index.html");
+        exit;
+    }
+
+    // If logout fails for some reason.... return the response as JSON
+    // Look I just copied this from my login.php
+    header('Content-Type: application/json');
+    echo json_encode($response);
+} else {
+    http_response_code(405);
+    echo json_encode([
+        "success" => false,
+        "message" => "Invalid request method."
+    ]);
+}
